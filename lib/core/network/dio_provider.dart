846b4d42af1ed2a36014909dedc51eab8be87e30
fkx-69 <125918024+fkx-69@users.noreply.github.com>
2025-06-19 16:15:57 +0000

    Add base URL configuration

--- lib/core/network/dio_provider.dart
+++ lib/core/network/dio_provider.dart
@@ -3,6 +3,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../config.dart';
 import '../interceptors/auth_interceptor.dart';
 
 final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
@@ -13,7 +14,7 @@ final dioProvider = Provider<Dio>((ref) {
   final secureStorage = ref.read(secureStorageProvider);
 
   final dio = Dio(BaseOptions(
-    baseUrl: 'http://192.168.137.243:8000/api/v1', // 🔁 Personnalise ceci
+    baseUrl: baseUrl,
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 10),
     contentType: 'application/json',
